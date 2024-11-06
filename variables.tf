5587bc7c3c663cc074fb13beea6ef447b935cc03
Manikantareddy96 <167765029+Manikantareddy96@users.noreply.github.com>
2024-11-06 17:28:09 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -17,5 +17,5 @@ variable "ami_id" {
 variable "bucketname" {
   description = "The name of the S3 bucket to create"
   type        = string
-  default     = "manikantahostinh-cloud"  #change Bucket name also
+  default     = "nagahosting"  #change Bucket name also
 }
