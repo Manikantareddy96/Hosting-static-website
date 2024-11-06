b7723ed0eb0d1d26a456be83be5626a281233413
Manikantareddy96 <167765029+Manikantareddy96@users.noreply.github.com>
2024-11-06 15:56:22 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,6 +1,6 @@
 #create s3 bucket
 resource "aws_s3_bucket" "mybucket" {
-  bucket = var.bucketname
+  bucket = nagahosting
 }
 resource "aws_s3_bucket_ownership_controls" "example" {
   bucket = aws_s3_bucket.mybucket.id
