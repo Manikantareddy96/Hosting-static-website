c2dd8534594c2564c33663b17a939f5d05308cc3
Manikantareddy96 <167765029+Manikantareddy96@users.noreply.github.com>
2024-11-06 17:21:18 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -5,15 +5,6 @@ resource "aws_s3_bucket_versioning" "example_bucket_versioning" {
   }
 }
 
-# Block public access settings to manage the visibility of the objects
-resource "aws_s3_bucket_public_access_block" "example" {
-  bucket = aws_s3_bucket.mybucket.id
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
-}
-
 # Create objects without ACLs since we are using BucketOwnerPreferred
 resource "aws_s3_object" "index" {
   bucket        = aws_s3_bucket.mybucket.id
